e433adb0fdcb8731bbc53ba48d5158f3d8d0ffef
net-runner <slazynski.j@gmail.com>
2019-11-24 22:35:25 +0100

    Login / register endpoints finished.

--- Server/api/login.php
+++ Server/api/login.php
@@ -1,12 +1,40 @@
 <?php
-//Login endpoint
+//Register endpoint
+
+//Adding headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-$conn = new mysqli('localhost', 'root', '', '4answers');
-if (!$conn) exit('Connection error');
+
 
 //Get posted data
-$data = json_decode(file_get_contents("php://input"));
+$un = $_GET['username'];
+$pw = $_GET['password'];
+$conn = new mysqli('localhost', 'root', '', '4answers');
+if (!$conn) exit('Connection error');
+$testQ = "SELECT * FROM users WHERE username='{$un}'";
+$rw = $conn->query($testQ) or die('Cannot fetch user');
+$res = $rw->fetch_row();
+
+//If there is a user with this username
+if (!empty($res)) {
+    if (password_verify($pw, $res[2])) {
+        echo json_encode(array(
+            'result' => "Success",
+            'message' => "Logged in."
+        ));
+    } else {
+        echo json_encode(array(
+            'result' => "Error",
+            'message' => "Passwords does not match."
+        ));
+    }
+} else {
+    //Handle error
+    echo json_encode(array(
+        'result' => "Error",
+        'message' => "User does not exist."
+    ));
+}
--- Server/api/register.php
+++ Server/api/register.php
@@ -1,12 +1,40 @@
 <?php
 //Register endpoint
+
+//Adding headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-$conn = new mysqli('localhost', 'root', '', '4answers');
-if (!$conn) exit('Connection error');
+
 
 //Get posted data
-$data = json_decode(file_get_contents("php://input"));
+$un = $_GET['username'];
+$pw = $_GET['password'];
+$conn = new mysqli('localhost', 'root', '', '4answers');
+if (!$conn) exit('Connection error');
+$testQ = "SELECT * FROM users WHERE username='{$un}'";
+$rw = $conn->query($testQ) or die('Cannot fetch user');
+$res = $rw->fetch_row();
+
+//If there is not a user with this username
+if (empty($res)) {
+    $hashed_password = password_hash($pw, PASSWORD_BCRYPT);
+    $dt = date("Y-m-d H:i:s");
+
+    $creation_query = "INSERT INTO users (username, password, userType, registerAt)
+    VALUES ('{$un}','{$hashed_password}','normal','{$dt}')";
+
+    $rw2 = $conn->query($creation_query) or die('Cannot add user');
+    echo json_encode(array(
+        'result' => "Success.",
+        'message' => "Account created."
+    ));
+} else {
+    //Handle error
+    echo json_encode(array(
+        'result' => "Error.",
+        'message' => "User already exists."
+    ));
+}
