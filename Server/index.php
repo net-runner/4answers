222a57c0624d2f59069bd3ab1d7a84e3af262eef
net-runner <slazynski.j@gmail.com>
2022-04-01 21:10:37 +0200

    Still fighting with db connection.

--- Server/index.php
+++ Server/index.php
@@ -1,43 +1,14 @@
 <?php
-
-use Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider;
-use Silex\Application;
-
-// $app->get('/api/questions/all', function () use ($app) {
-//     $st = $app['db']->prepare("SELECT *
-//     FROM questions");
-//     $st->execute();
-
-//     $questions = array();
-//     $q_arr = array();
-//     $q_arr['data'] = array();
-//     while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-//         $questions = json_decode($row[1]);
-//         $q_item = array(
-//             'id' => $row[0],
-//             'qType' => $row[2],
-//             'category' => $row[6],
-//             'author' => $row[4],
-//             'questions' => $questions->{'xd'},
-//             'createdAt' => $row[5],
-//             'qText' => $row[3]
-//         );
-//         array_push($q_arr['data'], $q_item);
-//     }
-//     return json_encode($q_arr);
-// });
-$app = new Application();
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 $dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(
-    new PDOServiceProvider('db'),
-    array(
-        'pdo.server' => array(
-            'driver'   => 'pgsql',
-            'user' => $dbopts["user"],
-            'password' => $dbopts["pass"],
-            'host' => $dbopts["host"],
-            'port' => $dbopts["port"],
-            'dbname' => ltrim($dbopts["path"], '/')
-        )
-    )
-);
+
+$db_handle = pg_connect("host=" .  $dbopts["host"] . "dbname=" . ltrim($dbopts["path"], '/') . "user=" . $dbopts["user"] . "password=" . $dbopts["pass"] . "port=" . $dbopts["port"]);
+
+if ($db_handle) {
+
+    echo 'Connection attempt succeeded.';
+} else {
+
+    echo 'Connection attempt failed.';
+}
