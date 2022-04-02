6e3ad9eba60e53a46556761976d2a31160f4035f
net-runner <slazynski.j@gmail.com>
2022-04-02 13:40:21 +0200

    Changing origin.

--- Server/index.php
+++ Server/index.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: https://4answers.netlify.app/');
+header('Access-Control-Allow-Origin: https://4answers.netlify.app');
 header('Content-Type: application/json');
 
 require __DIR__ . '/vendor/autoload.php';
